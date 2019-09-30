993acab99942b5f796ff28590105c71b5098f0ee
Gabriel <gabrielcostamoura1@gmail.com>
2019-09-30 13:05:50 -0300

    Login page ready

---
+++ src/view/pages/Login/Login.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from '../../assets/logo.png';
+import './style.css';
+export default function Login(){
+
+        return(
+                <div className="login-box">
+                
+                    <div className="login-header">
+                        <img  className="header-img" src={logo} alt="Logo"/>
+                        <h3   className="header-text">Acesse sua conta</h3>
+                    </div>
+                
+                    <div className="login-inputs">
+                        <input className="input" type="username" placeholder="Digite seu login"></input>
+                        <input className="input" type="password" placeholder="Digite sua senha"></input>
+                    </div>
+                    <div className="login-footer">
+
+                        <div className="checkBox">
+                            <input type="checkbox"/>
+                            <text> Manter conectado</text>
+                        </div>
+
+                        <div className="login-recover-pass">
+                            <a href="#">Esqueceu a senha?</a>
+                        </div>
+                    </div>
+
+                    <div className="login-button">
+                        <button className="button">Acessar conta</button>
+                    </div>
+
+                </div> 
+        );
+
+    }
