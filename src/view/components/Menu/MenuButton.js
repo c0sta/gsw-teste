6550516b8fba15965d33eff25d618d235d8a217d
c0sta <gabrielcostamoura1@gmail.com>
2019-10-04 20:10:59 -0300

    sidebar icons updated

--- src/view/components/Menu/MenuButton.js
+++ src/view/components/Menu/MenuButton.js
@@ -6,7 +6,7 @@ import {Link} from 'react-router-dom';
 const MenuButton = (props) => (
     <Link to={props.to} className="menu-box">
       <div className="menu-button-box">
-        <img src={props.src} alt="icon"/>
+        <img src={props.src} alt="icon" className="icon-menu"/>
         <button >{props.name}</button>
 
       </div>
