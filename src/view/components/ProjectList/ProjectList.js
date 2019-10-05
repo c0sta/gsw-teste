1d08e7e6f540210083c49dc2f1d519a36d3441a9
c0sta <gabrielcostamoura1@gmail.com>
2019-10-05 13:16:51 -0300

    list project updated

--- src/view/components/ProjectList/ProjectList.js
+++ src/view/components/ProjectList/ProjectList.js
@@ -2,6 +2,10 @@ import React, {useState, useEffect} from 'react';
 import './ProjectList.css';
 import Item from '../ItemList/ItemList.js';
 import api from '../../../services/api.js';
+
+import filter from '../../assets/dashboard/filter.png';
+import arrowDown from '../../assets/dashboard/arrow-down-light.png';
+
 export default function ProjectList(){
 
     const [data, setData] = useState([]);
@@ -18,18 +22,28 @@ export default function ProjectList(){
     }, []);
 
     return(
-        <div className="card-projectlist">
+        <div className="list-box">
             
-            <header>
-                <h1>Resultado do filtro</h1>
-                <input type="search"/>
+            <h1 className="list-title">Resultado do filtro</h1>
+
+            <header className="list-header">
+
+                <div className="list-filter">
+                    <img src={filter} alt="icon filter"/>
+                    <input type="search" placeholder="Projetos"/>
+                    <img src={arrowDown} alt="icon arrown down"/>
+                </div>
+                <div className="list-horas">
+                    <text>Horas lançadas</text>
+                    <text>Horas faltantes</text>
+                </div> 
             </header>
             <div className="list">
             { data.length > 0 ? (
 
                 <ul >    
                     { data.map( projeto => (
-                        <div className="item">
+                        <div className="list-item">
                         
                             <Item key={projeto.id} onClick={ () => setOpen(!isOpen)} criadoPor={projeto.criadoPor} projeto={projeto.projeto} entrega={projeto.entrega} horas_dedicadas={projeto.horas_dedicadas} horas_faltantes={projeto.horas_faltantes}/>
                         
@@ -39,15 +53,15 @@ export default function ProjectList(){
                                         comment => (
                                             <div className="item-obs">
                                                 <header className="item-obs-header">
-                                                <text>Observações</text>
-                                                <text>Data</text>
-                                                <text>Horas trabalhadas</text>
+                                                    <text>Observações</text>
+                                                    <text>Data</text>
+                                                    <text>Horas trabalhadas</text>
                                                 </header>
                                                 <div className="item-obs-body">
-                                                <text>Observações</text>
-                                                <text>Data</text>
+                                                    <text>Observações</text>
+                                                    <text>Data</text>
+                                                </div>
                                             </div>
-                                        </div>
                                         )
                                     )}        
                                 </div>
